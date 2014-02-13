6c5c0d213b19b04e3cf3a766f85e1ad3ea9743e0
stx <splusminusx@gmail.com>
2014-02-14 00:30:29 +0400

    base recipes

---
+++ cookbooks/oh_my_zsh/attributes/default.rb
@@ -0,0 +1,5 @@
+default['oh_my_zsh'][:users] = [
+  :login => 'vagrant',
+  :theme => 'dieter',
+  :plugins => ['git', 'django', 'virtualenvwrapper', 'python', 'heroku', 'git-hubflow', 'git-flow', 'git-extras', 'pip', 'node', 'nvm', 'ruby', 'command-not-found']
+]
