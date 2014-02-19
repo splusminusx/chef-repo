abf95f91bf48babbe157b78716b4f25098f4aaf2
stx <splusminusx@gmail.com>
2014-02-20 00:15:38 +0400

    old cookbooks removed

--- cookbooks/livetex-qa-site/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'livetex-qa-site'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures livetex-qa-site'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
--- cookbooks/oh-my-zsh/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default['oh_my_zsh'][:users] = [
-  :login => 'vagrant',
-  :theme => 'dieter',
-  :plugins => ['git', 'django', 'virtualenvwrapper', 'python', 'heroku', 'git-hubflow', 'git-flow', 'git-extras', 'pip', 'node', 'nvm', 'ruby', 'command-not-found']
-]
